384043366a702fb2d6f792bfb94170769ed9a1fb
lakshmi863 <sathya.lakshmipathi@gmail.com>
2026-05-28 10:41:36 +0530

    path change

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
-// Import components
+// Import component
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import Login from './components/Auth/Login';
